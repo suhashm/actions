ac1158741d0ed605504a6c5ba37c7e943339634d
Suhas <suhas@Suhass-MacBook-Pro.local>
2019-09-19 02:52:07 -0700

    entry fix

--- neo4j-golang-build/entrypoint.sh
+++ neo4j-golang-build/entrypoint.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 set -e
-
+echo "---> Starting"
 if [[ -z "$GITHUB_WORKSPACE" ]]; then
   echo "Set the GITHUB_WORKSPACE env variable."
   exit 1
@@ -15,7 +15,7 @@ fi
 
 root_path="/go/src/github.com/$GITHUB_REPOSITORY"
 
-echo "----> Setting up Go repository: $GITHUB_REPOSITORY $GITHUB_WORKSPACE $root_path"
+echo "----> Setting up Go repository"
 # mkdir -p $release_path
 mkdir -p $root_path
 cp -a $GITHUB_WORKSPACE/* $root_path/

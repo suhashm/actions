79d578589e891f6daf69b79b831e878b7bb8c71e
Suhas <suhas@Suhass-MacBook-Pro.local>
2019-09-19 02:47:53 -0700

    fixing go code copy

--- neo4j-golang-build/entrypoint.sh
+++ neo4j-golang-build/entrypoint.sh
@@ -1,6 +1,29 @@
+
 #!/bin/bash
 
 set -e
 
-go build ./...
+if [[ -z "$GITHUB_WORKSPACE" ]]; then
+  echo "Set the GITHUB_WORKSPACE env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_REPOSITORY" ]]; then
+  echo "Set the GITHUB_REPOSITORY env variable."
+  exit 1
+fi
+
+root_path="/go/src/github.com/$GITHUB_REPOSITORY"
+
+echo "----> Setting up Go repository: $GITHUB_REPOSITORY $GITHUB_WORKSPACE $root_path"
+# mkdir -p $release_path
+mkdir -p $root_path
+cp -a $GITHUB_WORKSPACE/* $root_path/
+cd $root_path
+
+go build -o main .
+
 go test -v -short -race ./...
+
+echo "----> Build is complete"
+ls -al

7e80a1331574f2992e88678cdc77632c545403fd
Suhas <suhas@Suhass-MacBook-Pro.local>
2019-09-19 03:07:33 -0700

    docker

--- neo4j-golang-build/entrypoint.sh
+++ neo4j-golang-build/entrypoint.sh
@@ -1,8 +1,7 @@
-
 #!/bin/bash
 
 set -e
-echo "---> Starting"
+
 if [[ -z "$GITHUB_WORKSPACE" ]]; then
   echo "Set the GITHUB_WORKSPACE env variable."
   exit 1
@@ -14,16 +13,26 @@ if [[ -z "$GITHUB_REPOSITORY" ]]; then
 fi
 
 root_path="/go/src/github.com/$GITHUB_REPOSITORY"
+release_path="$GITHUB_WORKSPACE/.release"
+repo_name="$(echo $GITHUB_REPOSITORY | cut -d '/' -f2)"
+targets=${@-"darwin/amd64 darwin/386 linux/amd64 linux/386 windows/amd64 windows/386"}
 
 echo "----> Setting up Go repository"
-# mkdir -p $release_path
+mkdir -p $release_path
 mkdir -p $root_path
 cp -a $GITHUB_WORKSPACE/* $root_path/
 cd $root_path
 
-go build -o main .
+for target in $targets; do
+  os="$(echo $target | cut -d '/' -f1)"
+  arch="$(echo $target | cut -d '/' -f2)"
+  output="${release_path}/${repo_name}_${os}_${arch}"
 
-go test -v -short -race ./...
+  echo "----> Building project for: $target"
+  GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -o $output
+  zip -j $output.zip $output > /dev/null
+done
 
-echo "----> Build is complete"
+echo "----> Build is complete. List of files at $release_path:"
+cd $release_path
 ls -al
